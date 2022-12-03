86d19de23033dad6ed63ea388087388fc08fe027
Dmitry-Murmansk <119297978+Dmitry-Murmansk@users.noreply.github.com>
2022-12-03 20:21:52 +0300

    Username Masha

---
+++ Examle/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользова: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "нина")
+{
+    Console.WriteLine("Ура, ура это же Нина");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
