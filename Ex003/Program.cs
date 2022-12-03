ccabddd089f2c6c2f08baaf1721ad1c806878a8c
Dmitry-Murmansk <119297978+Dmitry-Murmansk@users.noreply.github.com>
2022-12-03 19:51:32 +0300

    Random

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 35;
-int numberD = 9;
+﻿int numberA = new Random() .Next(1, 10); // 1 2 3 ... 9
+Console.WriteLine(numberA);
+int numberD = new Random() .Next(1, 10);
+Console.WriteLine(numberD);
 int result = numberA + numberD;
 Console.WriteLine(result);
